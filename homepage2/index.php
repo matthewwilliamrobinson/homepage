6705b891534503286230c9c11af83e48af7e8b38
Samuel Phillips <samuel.phillips@sbwebstore.com>
2016-07-14 18:37:35 +0000

    Style change

--- homepage2/index.php
+++ homepage2/index.php
@@ -50,30 +50,31 @@ spit_template(function () { ?>
     </div><!-- End Container -->
         
     <!-- call to action -->
-    <section>
+    <section id=contact>
         <div class="well">
             <div class="container text-center">
                 <h3>Contact Us</h3>
                 <p>Enter your name, your email, and a short message</p>
                 
-                <form action="" class="form-inline ajax-mail">
-                    <div class="form-group">
-                        <label for="full-name">Subscribe</label>
+                <form class="ajax-mail col-md-8 center-block text-left" style="float:none">
+                    <div class="form-group col-sm-6">
+                        <label for="full-name">Your Name</label>
                         <input type="text" class="form-control" id="full-name" name="full-name" placeholder="Your Name">
                         <label for="full-name" class=ajax-mail-errors>You must enter your name.</label>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-sm-6">
                         <label for="reply-to">Email address</label>
                         <input type="text" class="form-control" id="reply-to" name="reply-to" placeholder="Enter your Email">
                         <label for="reply-to" class=ajax-mail-errors>Make sure you enter a valid email address!</label>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group col-sm-12">
                         <label for=body>Message</label>
                         <textarea class=form-control id=body name=body placeholder="Send us a message..."></textarea>
                         <label for=body class=ajax-mail-errors>Make sure you enter a message.</label>
                     </div>
-                    <button type="submit" class="btn btn-default js-button" disabled
-                        data-desired-content="Submit">This form requires Javascript</button>
+                    <div class="col-sm-12 text-right">
+                        <button type="submit" class="btn btn-primary js-button" disabled data-desired-content="Submit">This form requires Javascript</button>
+                    </div>
                     <hr>
                     <div class=ajax-mail-result-message></div>
                 </form>
